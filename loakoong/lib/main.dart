3892bfdfc3683592e228cd11ac589db7f7449075
MoonhaloSUZ <vvdbal@naver.com>
2023-05-02 23:56:23 +0900

    build 수정

--- loakoong/lib/main.dart
+++ loakoong/lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //override build //
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
