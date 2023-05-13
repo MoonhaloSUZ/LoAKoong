e4d791eb30957edf02b5a36917441b13c488a713
MoonhaloSUZ <vvdbal@naver.com>
2023-05-13 19:35:59 +0900

    factory 적용

--- loakoong/lib/model/loakoong_model.dart
+++ loakoong/lib/model/loakoong_model.dart
@@ -1,9 +1,40 @@
 class LoAKoongModel {
-  final String charactername, characterlevel;
+  final String serverName;
+  final String characterName;
+  final int characterLevel;
+  final String characterClassName;
+  final String itemAvgLevel;
+  final String itemMaxLevel;
 
-  LoAKoongModel.fromJson(Map<String, dynamic> json)
-      : charactername = json['CharacterName'],
-        characterlevel = json['CharacterLevel'];
+  LoAKoongModel({
+    required this.serverName,
+    required this.characterName,
+    required this.characterLevel,
+    required this.characterClassName,
+    required this.itemAvgLevel,
+    required this.itemMaxLevel,
+  });
+
+  factory LoAKoongModel.fromJson(Map<String, dynamic> json) {
+    return LoAKoongModel(
+      itemMaxLevel: json['ItemMaxLevel'],
+      serverName: json['ServerName'],
+      characterName: json['CharacterName'],
+      characterLevel: json['CharacterLevel'],
+      characterClassName: json['CharacterClassName'],
+      itemAvgLevel: json['ItemAvgLevel'],
+    );
+  }
+
+  factory LoAKoongModel.fromList(List<dynamic> jsonList) {
+    if (jsonList.isEmpty) {
+      throw Exception('Empty response');
+    }
+
+    final jsonData = jsonList.first as Map<String, dynamic>;
+    return LoAKoongModel.fromJson(jsonData);
+  }
+
   //named constructor 이용
   //api_service.dart 에서 api json 값을 받아오고, 여기서 초기화
   //
