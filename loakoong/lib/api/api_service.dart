c7c1458d4473eb146f58028c450a96d992490b41
MoonhaloSUZ <vvdbal@naver.com>
2023-07-17 00:31:49 +0900

    response body 응답 라인 수정

--- loakoong/lib/api/api_service.dart
+++ loakoong/lib/api/api_service.dart
@@ -1,18 +1,49 @@
 import 'dart:convert';
 
 import 'auth.dart';
-import 'package:loakoong/model/loakoong_model.dart';
 import 'package:http/http.dart' as http;
 
 class LostArkAPI {
-  static Future<LoAKoongModel> getCharacterProfile(String userName) async {
+  static Future<List> getCharacterProfile(String userName) async {
+    const isSearched = false;
+
     Map<String, String> header = {
       'accept': 'application/json',
       'authorization': 'bearer $loaAuth',
     };
 
     var loaurl = 'https://developer-lostark.game.onstove.com';
+    final url = Uri.parse("$loaurl/characters/$userName/siblings");
+    final response = await http.get(url, headers: header);
+    //final responsebody = jsonDecode(response.body);
 
+    if (response.statusCode == 200) {
+      final responsebody = jsonDecode(response.body) as List<dynamic>;
+      //예외사항 : statusCode 200이 들어오더라도, jsonDecode(response.body)에 Null값이 들어올 수 있음
+
+      responsebody.sort((a, b) => b['ItemMaxLevel']
+          .replaceAll(',', '')
+          .compareTo(a['ItemMaxLevel'].replaceAll(',', '')));
+
+      final characterNames =
+          responsebody.map((item) => item['CharacterName']).toList();
+
+      print(responsebody);
+      print(characterNames);
+
+      return characterNames;
+    } else {
+      throw Exception('오오~류');
+    }
+  }
+
+  static Future<List> getCharacterLevel(String userName) async {
+    Map<String, String> header = {
+      'accept': 'application/json',
+      'authorization': 'bearer $loaAuth',
+    };
+
+    var loaurl = 'https://developer-lostark.game.onstove.com';
     final url = Uri.parse("$loaurl/characters/$userName/siblings");
     final response = await http.get(url, headers: header);
     //final responsebody = jsonDecode(response.body);
@@ -22,28 +53,18 @@ class LostArkAPI {
     if (response.statusCode == 200) {
       //예외사항 : statusCode 200이 들어오더라도, jsonDecode(response.body)에 Null값이 들어올 수 있음
 
-      if (responsebody != null) {
-        // responsebody
-        //     .sort((a, b) => b['ItemMaxLevel'].compareTo(a['ItemMaxLevel']));
-        responsebody.sort((a, b) => b['ItemMaxLevel']
-            .replaceAll(',', '')
-            .compareTo(a['ItemMaxLevel'].replaceAll(',', '')));
+      responsebody.sort((a, b) => b['ItemMaxLevel']
+          .replaceAll(',', '')
+          .compareTo(a['ItemMaxLevel'].replaceAll(',', '')));
 
-        final characterNames =
-            responsebody.map((item) => item['CharacterName']).toList();
+      final characterLevels =
+          responsebody.map((item) => item['ItemMaxLevel']).toList();
 
-        print(responsebody);
+      // print(responsebody);
+      // print(characterNames);
+      print(characterLevels);
 
-        return LoAKoongModel.fromList(responsebody);
-      } else {
-        return LoAKoongModel(
-            serverName: '캐릭터를 찾을 수 없어요 Ooㅠ_ㅠoO',
-            characterName: '캐릭터를 찾을 수 없어요 Ooㅠ_ㅠoO',
-            characterLevel: 0,
-            characterClassName: '캐릭터를 찾을 수 없어요 Ooㅠ_ㅠoO',
-            itemAvgLevel: '캐릭터를 찾을 수 없어요 Ooㅠ_ㅠoO',
-            itemMaxLevel: '캐릭터를 찾을 수 없어요 Ooㅠ_ㅠoO');
-      }
+      return characterLevels;
     } else {
       throw Exception('오오~류');
     }
