3c886c59b1388ac48bf2e56818451823699c97b8
MoonhaloSUZ <vvdbal@naver.com>
2023-10-15 18:20:02 +0900

    main 페이지의 loakoong 값과 selectValue(토글 값이 true인, 즉 선택된 캐릭터를 ListView로 뿌려주기

--- loakoong/lib/screen/character_choice_screen.dart
+++ loakoong/lib/screen/character_choice_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-//import 'package:loakoong/main.dart';
 
-class ScreenOfCharacterChoice extends StatelessWidget {
-  final selectValue;
-  const ScreenOfCharacterChoice({required this.selectValue, super.key});
+class ScreenOfCharacterChoice extends StatefulWidget {
+  final List<int> selectValue;
+  final Future<List<dynamic>> loakoong;
 
+  const ScreenOfCharacterChoice(
+      {required this.selectValue, required this.loakoong, super.key});
+
+  @override
+  State<ScreenOfCharacterChoice> createState() =>
+      _ScreenOfCharacterChoiceState();
+}
+
+class _ScreenOfCharacterChoiceState extends State<ScreenOfCharacterChoice> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -27,12 +35,46 @@ class ScreenOfCharacterChoice extends StatelessWidget {
                 //color: Colors.purple,
                 icon: const Icon(Icons.cached_rounded)),
           ),
-          body: Column(
-            children: [
-              Text(
-                '$selectValue.to',
-              ),
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  '선택된 토글 인덱스는 ${widget.selectValue}입니다',
+                ),
+
+                ///*********************************///
+                FutureBuilder<List<dynamic>>(
+                  future: widget.loakoong,
+                  //loakoong에 저장된 값은 메인 페이지에서 설정한 캐릭터 이름 값임(String 형식이 아님 Instance of'Future<list<dynamic>>'임)
+                  //
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final data = snapshot.data ?? [];
+                      //return Text('데이터 로드 성공: $data');
+                      return ListView.separated(
+                        shrinkWrap: true,
+                        itemCount: widget.selectValue.length,
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            height: 100,
+                            //color: Colors.amber,
+                            child: Center(
+                                child: Text(
+                                    ' ${data[widget.selectValue[index]]}')),
+                          );
+                        },
+
+                        //최상단과 최하단에 구분선을 추가하는 것부터 시작~
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const Divider(),
+                      );
+                    }
+                    // 데이터 로딩 중인 경우 표시할 위젯
+                    return const CircularProgressIndicator();
+                  },
+                )
+              ],
+            ),
           )),
     );
   }
